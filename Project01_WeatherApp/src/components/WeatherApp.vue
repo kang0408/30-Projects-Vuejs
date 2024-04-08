d94df1e2c8037af1ee74aee3b4b9ff15212a65dc
= <trandanhkhang482004@gamil.com>
2024-04-08 22:51:56 +0700

    Project01_WeatherApp

---
+++ Project01_WeatherApp/src/components/WeatherApp.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, reprehenderit.</p>
+    </div>
+</template>
+<style lang="scss">
+
+p {
+    color: red;
+}
+</style>
